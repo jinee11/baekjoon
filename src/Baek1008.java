3aaa225797bf07a6f933542a0acc700b59aca50c
JinSeon Lee <jineelee11@gmail.com>
2023-05-02 17:36:59 +0900

    [백준]새싹푸는중

---
+++ src/Baek1008.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Baek1008 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double multi = a/b;
+
+        System.out.println(multi);
+
+
+    }
+
+}
